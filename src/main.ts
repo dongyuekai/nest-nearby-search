5ccb0ad6afd5cea0ad5b2fcb16119d299b1c8168
dongyuekai <457545648@qq.com>
2024-08-18 15:26:58 +0800

    feat: 实现前端调用接口

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // 指定public目录为静态文件的目录
+  app.useStaticAssets('public');
+
   await app.listen(3000);
 }
 bootstrap();
